115838da88c7dc09bcb309a8e2450c93821068e1
mahmoudelhamrawi <mahmoudelhamrawi5@gmail.com>
2024-01-13 07:53:01 +0200

    add open new account feature

--- src/main/java/Page/P04_OpenAccountPage.java
+++ src/main/java/Page/P04_OpenAccountPage.java
@@ -20,37 +20,47 @@ public class P04_OpenAccountPage extends pageBase{
  private final By createBtn = By.cssSelector("input[class=\"button\"]");
  private final By assertOnShowResult = By.id("rightPanel");
  private final By assertAfterCreate = By.cssSelector("div[id=\"rightPanel\"] div h1");
+    ///accountSaving
+    private final By selSavingTag =By.id("type");
+
+  private final By accountNumber= By.xpath("//a[@id=\"newAccountId\"]");
 
 
- public void goToAccountPageCreation()
+
+
+    public void goToAccountPageCreation()
  {
      clickEle(driver.findElement(openAccountLink));
  }
-
- public WebElement assetTitle()
+    public WebElement assetTitle()
  {
      return driver.findElement(assertOnTitle);
  }
- public WebElement myAccountMain()
+     public WebElement myAccountMain()
  {
      return  driver.findElement(accountMain);
  }
-
- public void createAccount()
+    public void createAccount()
  {
      clickEle(driver.findElement(createBtn));
  }
-
-  public WebElement assertOnShowRes()
+    public WebElement assertOnShowRes()
   {
       return driver.findElement(assertOnShowResult);
   }
-
-    public WebElement assertAccountOpened()
+     public WebElement assertAccountOpened()
     {
         return  driver.findElement(assertAfterCreate);
     }
 
 
+    public WebElement savingAcc()
+    {
+        return driver.findElement(selSavingTag);
+    }
 
+    public WebElement numberAcc()
+    {
+        return driver.findElement(accountNumber);
+    }
 }
---
+++ src/test/java/TestCases/TC04_OpenNewAccountSaving.java
@@ -0,0 +1,78 @@
+package TestCases;
+
+import Data.readJsonData;
+import Page.P03_LoginPage;
+import Page.P04_OpenAccountPage;
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.time.Duration;
+
+public class TC04_OpenNewAccountSaving extends testBase
+{
+
+
+    P04_OpenAccountPage openAccountPage ;
+
+    P03_LoginPage loginPage ;
+    public static  String  accNumSaving ;
+
+    Data.readJsonData readJsonData ;
+    @DataProvider(name = "jsonData")
+    public Object[] testDataForRegister() throws IOException, ParseException {
+        readJsonData = new readJsonData();
+        return readJsonData.testDataForSuccessfulRegister();
+    }
+
+
+    @Test(dataProvider = "jsonData")
+    public void createNewAccountSaving(String data) throws InterruptedException {
+        //login
+        String[] users = data.split(",");
+        loginPage = new P03_LoginPage(driver);
+        loginPage.fillLoginForm(users[7],users[8]);
+        Assert.assertEquals("Accounts Overview",loginPage.assertTit().getText());
+
+        WebDriverWait wait = new WebDriverWait(driver , Duration.ofSeconds(40));
+        wait.until(ExpectedConditions.visibilityOf(loginPage.tableView()));
+        Assert.assertEquals("Account",loginPage.assertOnContentOfHead().getText());
+
+        //open Account
+        openAccountPage = new P04_OpenAccountPage(driver);
+        openAccountPage.goToAccountPageCreation();
+        System.out.println(openAccountPage.assetTitle().getText());
+        Assert.assertEquals("Open New Account",openAccountPage.assetTitle().getText());
+
+        wait.until(ExpectedConditions.visibilityOf(openAccountPage.myAccountMain()));
+        Thread.sleep(3000);
+
+        Select sel = new Select(openAccountPage.savingAcc());
+        sel.selectByValue("1");
+
+
+        openAccountPage.createAccount();
+        Thread.sleep(3000);
+        System.out.println(openAccountPage.assertAccountOpened().getText());
+//        wait.until(ExpectedConditions.visibilityOf(openAccountPage.assertOnShowRes()));
+        Assert.assertEquals("Account Opened!" , openAccountPage.assertAccountOpened().getText());
+        Thread.sleep(3000);
+
+        //take account number
+        accNumSaving = openAccountPage.numberAcc().getText();
+        System.out.println(accNumSaving);
+
+
+    }
+
+
+
+
+
+
+}
