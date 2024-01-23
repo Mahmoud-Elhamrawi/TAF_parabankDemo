933d43c2dcf4c7980ca5454b79cbcf93ed8027c8
mahmoudelhamrawi <mahmoudelhamrawi5@gmail.com>
2024-01-23 16:50:54 +0200

    add mvn command and allure-report and profiling seeting

--- src/test/java/TestCases/testBase.java
+++ src/test/java/TestCases/testBase.java
@@ -3,6 +3,8 @@ package TestCases;
 import Helper.screenShotFail;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 
@@ -12,9 +14,18 @@ public class testBase {
     public WebDriver driver ;
 
     @BeforeClass
-    public void openingBrowser()
+    @Parameters({"browser"})
+    public void openingBrowser(@Optional("chrome") String browserName)
     {
-        driver = new ChromeDriver();
+        if(browserName.equalsIgnoreCase("chrome"))
+        {
+            driver = new ChromeDriver();
+        } else if (browserName.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+        }else if (browserName.equalsIgnoreCase("edge"))
+        {
+            driver =new EdgeDriver();
+        }
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.MILLISECONDS);
         driver.manage().window().maximize();
         driver.get("https://parabank.parasoft.com/parabank/index.htm?ConnType=JDBC");
